326178d8bad64701cc7741090fe4edcbf6d0fad4
YOUR NAME <137875510+wachagon@users.noreply.github.com>
2026-05-13 19:06:24 +0900

    divide
    Plate.csとWashPlate.csを裂きました。

--- SaraArai/Assets/Scripts/GameManager.cs
+++ SaraArai/Assets/Scripts/GameManager.cs
@@ -7,8 +7,12 @@ public class GameManager : MonoBehaviour
 {
     private int money = 10;
     private int plateCost = 1;
+    private GameObject currentWashPlate;
     [SerializeField] private TextMeshProUGUI moneyText;
-    [SerializeField] private GameObject platePrefab;
+    [SerializeField] private GameObject platePrefab;//te-buru no ue no sara
+    [SerializeField] private GameObject washPlatePrefab;//arau toki no sara\
+    [SerializeField] private Transform washPoint;
+
     [SerializeField] private Transform plateSpawnMinPoint;
     [SerializeField] private Transform plateSpawnMaxPoint;
     [SerializeField] private float plateSlideDuration = 0.5f;
@@ -40,6 +44,16 @@ public class GameManager : MonoBehaviour
         UpdateMoneyText();
     }
 
+    public void StartWashing(Plate tablePlate)
+    {
+        if (currentWashPlate != null)
+        {
+            return;
+        }
+        currentWashPlate = Instantiate(washPlatePrefab, washPoint.position, Quaternion.identity);
+        currentWashPlate.transform.localScale = new Vector3(3f, 3f, 1f);
+    }
+
     public void OnClickSpawnPlateButton()
     {
         if (money < plateCost)
@@ -54,10 +68,8 @@ public class GameManager : MonoBehaviour
 
     void SpawnPlate()
     {
-        float randomX = Random.Range(plateSpawnMinPoint.position.x, plateSpawnMaxPoint.position.x);
-        float randomY = Random.Range(plateSpawnMinPoint.position.y, plateSpawnMaxPoint.position.y);
-        randomX = (randomX + randomX)/2f;
-        randomY = (randomY + randomY)/2f;
+        float randomX = (Random.Range(plateSpawnMinPoint.position.x, plateSpawnMaxPoint.position.x) + Random.Range(plateSpawnMinPoint.position.x, plateSpawnMaxPoint.position.x)) / 2f;
+        float randomY = (Random.Range(plateSpawnMinPoint.position.y, plateSpawnMaxPoint.position.y) + Random.Range(plateSpawnMinPoint.position.y, plateSpawnMaxPoint.position.y)) / 2f;
         Vector3 spawnPosition = new Vector3(randomX, randomY, plateSpawnMinPoint.position.z);
         Vector3 startPosition = new Vector3(spawnPosition.x * 0.5f, spawnPosition.y + plateStartYOffset, spawnPosition.z);
         GameObject plate = Instantiate(platePrefab, startPosition, Quaternion.identity);
@@ -73,5 +85,11 @@ public class GameManager : MonoBehaviour
             }
             plateTransform.position = endPos;
         }
+    }
+
+    public void FinishWashing()
+    {
+        AddMoney(10);
+        currentWashPlate = null;
     }
 }
--- SaraArai/Assets/Scripts/Plate.cs
+++ SaraArai/Assets/Scripts/Plate.cs
@@ -5,138 +5,14 @@ using UnityEngine.Rendering;
 
 public class Plate : MonoBehaviour
 {
-    private SortingGroup sortingGroup;
-    [SerializeField] private SpriteRenderer dirtRenderer;
-
-    private Texture2D dirtTexture;
     private GameManager gameManager;
-    private bool isWashing = false;
-
-    [SerializeField] private int brushRadius = 16;
-    [SerializeField] private float cleanPower = 0.15f;
-
-    private float initialDirtAmount;
-    private bool isCleaned = false;
-
-    [SerializeField] private float cleanThreshold = 0.1f;
-
-    [SerializeField] private Transform spongeVisual;
-
-
     void Awake()
     {
-        sortingGroup = GetComponent<SortingGroup>();
         gameManager = FindObjectOfType<GameManager>();
     }
 
     public void OnMouseDown()
     {
-        GameObject washPoint = GameObject.Find("WashPoint");
-        transform.position = washPoint.transform.position;
-        transform.localScale = new Vector3(3f, 3f, 1f);
-        sortingGroup.sortingOrder = 100;
-        isWashing = true;
-        spongeVisual.gameObject.SetActive(true);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        Texture2D originalTexture = dirtRenderer.sprite.texture;
-
-        dirtTexture = new Texture2D(originalTexture.width, originalTexture.height, TextureFormat.RGBA32, false);
-        dirtTexture.filterMode = FilterMode.Point;
-        dirtTexture.wrapMode = TextureWrapMode.Clamp;
-
-        dirtTexture.SetPixels(originalTexture.GetPixels());
-        dirtTexture.Apply();
-
-        dirtRenderer.sprite = Sprite.Create(dirtTexture, dirtRenderer.sprite.rect, new Vector2(0.5f, 0.5f), dirtRenderer.sprite.pixelsPerUnit);
-
-        initialDirtAmount = CalculateDirtAmount();
-        spongeVisual.gameObject.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!isWashing)
-        {
-            return;
-        }
-
-        MoveSpongeToMouse();
-
-        if (Input.GetMouseButton(0)) // 左クリックを押している間
-        {
-            CleanAtMousePosition();
-        }
-    }
-
-    private void CleanAtMousePosition()
-    {
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 localPosition = dirtRenderer.transform.InverseTransformPoint(mouseWorldPosition);
-        Sprite sprite = dirtRenderer.sprite;
-        float pixelsPerUnit = sprite.pixelsPerUnit;
-        Rect rect = sprite.rect;
-        int centerX = Mathf.RoundToInt(localPosition.x * pixelsPerUnit + rect.width / 2f);
-        int centerY = Mathf.RoundToInt(localPosition.y * pixelsPerUnit + rect.height / 2f);
-        for (int y = -brushRadius; y <= brushRadius; y++)
-        {
-            for (int x = -brushRadius; x <= brushRadius; x++)
-            {
-                if (x * x + y * y > brushRadius * brushRadius)
-                {
-                    continue;
-                }
-                int pixelX = centerX + x;
-                int pixelY = centerY + y;
-                if (pixelX < 0 || pixelX >= dirtTexture.width || pixelY < 0 || pixelY >= dirtTexture.height)
-                {
-                    continue;
-                }
-                Color color = dirtTexture.GetPixel(pixelX, pixelY);
-                color.a -= cleanPower;
-                color.a = Mathf.Clamp01(color.a);
-                dirtTexture.SetPixel(pixelX, pixelY, color);
-            }
-        }
-        dirtTexture.Apply();
-        CheckCleaned();
-    }
-
-    private float CalculateDirtAmount()
-    {
-        Color[] pixels = dirtTexture.GetPixels();
-        float dirtAmount = 0f;
-        foreach (Color pixel in pixels)
-        {
-            dirtAmount += pixel.a;
-        }
-        return dirtAmount;
-    }
-
-    private void CheckCleaned()
-    {
-        if (isCleaned)
-        {
-            return;
-        }
-        float currentDirtAmount = CalculateDirtAmount();
-        if (currentDirtAmount / initialDirtAmount <= cleanThreshold)
-        {
-            isCleaned = true;
-            isWashing = false;
-            spongeVisual.gameObject.SetActive(false);
-            gameManager.AddMoney(10); // 皿をきれいにしたら10円追加
-            Debug.Log("皿がきれいになりました！");
-        }
-    }
-
-    private void MoveSpongeToMouse()
-    {
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPosition.z = spongeVisual.position.z;
-        spongeVisual.position = mouseWorldPosition;
+        gameManager.StartWashing(this);
     }
 }
