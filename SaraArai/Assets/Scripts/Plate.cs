db61326f6ef521105dbcd864dedc4d5b4986d591
YOUR NAME <137875510+wachagon@users.noreply.github.com>
2026-05-17 15:36:28 +0900

    Drug on the table

--- SaraArai/Assets/Scripts/Plate.cs
+++ SaraArai/Assets/Scripts/Plate.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class Plate : MonoBehaviour
 {
     private GameManager gameManager;
+    private Vector3 dragOffset;
+    private Vector3 mouseDownPosition;
+    private bool isDragging = false;
+    
+    [SerializeField] private float clickTimeLimit = 0.2f;
+    [SerializeField] private float dragStartDistance = 0.2f;
+    private float mouseDownTime;
+
     void Awake()
     {
         gameManager = FindObjectOfType<GameManager>();
@@ -13,6 +22,45 @@ public class Plate : MonoBehaviour
 
     public void OnMouseDown()
     {
-        gameManager.StartWashing(this);
+        mouseDownTime = Time.time;
+        mouseDownPosition = GetMouseWorldPosition();
+        dragOffset = transform.position - mouseDownPosition;
+        isDragging = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+        float distance = Vector3.Distance(mouseDownPosition, mousePosition);
+
+        if (distance >= dragStartDistance)
+        {
+            isDragging = true;
+        }
+
+        if (isDragging)
+        {
+            transform.position = mousePosition + dragOffset;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        float holdTime = Time.time - mouseDownTime;
+
+        if (!isDragging && holdTime <= clickTimeLimit)
+        {
+            gameManager.StartWashing(this);
+        }
+        
+        isDragging = false;
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z;
+        return mousePosition;
     }
 }
